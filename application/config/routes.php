08113bbd286c6d1647ebf73a1397193a4b4fae8f
zsebtanar <zsebtanar@gmail.com>
2016-01-21 22:39:57 +0100

    default route changed Signed-off-by: zsebtanar <zsebtanar@gmail.com>

--- application/config/routes.php
+++ application/config/routes.php
@@ -49,6 +49,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
-$route['default_controller'] = 'view/subtopic';
+$route['default_controller'] = 'view/main';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
