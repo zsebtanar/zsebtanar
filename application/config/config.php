3d7cd166bd4fc1a502ab873d7869d225017b78a4
zsebtanar <zsebtanar@gmail.com>
2016-02-03 20:53:09 +0100

    config.php modified Signed-off-by: zsebtanar <zsebtanar@gmail.com>

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://zsebtanar.hu/public';
+$config['base_url'] = 'http://localhost/zsebtanar4/public';
 
 /*
 |--------------------------------------------------------------------------
@@ -370,8 +370,8 @@ $config['encryption_key'] = '';
 $config['sess_driver'] = 'files';
 $config['sess_cookie_name'] = 'ci_session';
 $config['sess_expiration'] = 7200;
-$config['sess_save_path'] = BASEPATH . 'cache/';
-// $config['sess_save_path'] = NULL;
+// $config['sess_save_path'] = BASEPATH . 'cache/';
+$config['sess_save_path'] = NULL;
 $config['sess_match_ip'] = FALSE;
 $config['sess_time_to_update'] = 300;
 $config['sess_regenerate_destroy'] = FALSE;
