f2cfe3beb2312250274921a5a377f29a4d492d91
Zsebtanár <zsebtanar@gmail.com>
2016-03-23 01:09:51 +0100

    change display Signed-off-by: Zsebtanár <zsebtanar@gmail.com>

--- application/views/Modal/Info.php
+++ application/views/Modal/Info.php
@@ -3,32 +3,29 @@
 		<div class="modal-content">
 			<div class="modal-header">
 				<button type="button" class="close" data-dismiss="modal">&times;</button>
-				<h1 class="modal-title">A honlapról</h1>
+				<h3 class="modal-title">Jelmagyarázat</h3>
 			</div>
 			<div class="modal-body">
-				<p>Ez a honlap azoknak készült, akiknek nem megy a matek.</p>
-				<p>Észrevételeket, javaslatokat a <b>zsebtanar@gmail.com</b>-ra lehet küldeni.</p>
-				<p>Jó tanulást kívánok!</p>
-				<p class="text-right"><i>Szabó Viktor</i></p>
-				<h1 class="modal-title">Jelmagyarázat</h1>
-				<h3><b><img class="img_main" src="<?php echo base_url();?>assets/images/coin.png" alt="coin" width="50px">&nbsp;Arany</b></h3>
+				<h3><b><img class="img_main" src="<?php echo base_url();?>assets/images/coin.png" alt="coin" width="40px">&nbsp;Arany</b></h3>
 				<ul>
 					<li>Minden jó feladatmegoldásért kapsz aranyat.</li>
 					<li>Minél kevesebb segítséget használsz fel a megoldáshoz, annál többet.</li>
 					<li>Ha szintet lépsz, vagy elvégzel egy feladatot, extra aranyat kapsz.</li>
 				</ul>
-				<h3><b><img class="img_main" src="<?php echo base_url();?>assets/images/shield.png" alt="shield" width="50px">&nbsp;Pajzs</b></h3>
+				<h3><b><img class="img_main" src="<?php echo base_url();?>assets/images/shield.png" alt="shield" width="40px">&nbsp;Pajzs</b></h3>
 				<ul>
 					<li>Ha szintet lépsz egy feladatnál, egy pajzsot kapsz.</li>
 				</ul>
-				<h3><b><img class="img_main" src="<?php echo base_url();?>assets/images/trophy.png" alt="trophy" width="50px">&nbsp;Trófea</b></h3>
+				<h3><b><img class="img_main" src="<?php echo base_url();?>assets/images/trophy.png" alt="trophy" width="40px">&nbsp;Trófea</b></h3>
 				<ul>
 					<li>Ha elvégzel egy feladatot, egy trófeát kapsz jutalomként.</li>
 				</ul>
 				<br />
 			</div>
 			<div class="modal-footer">
+				<p class="small pull-right">A <b>Zsebtanár</b> egy interaktív matematikai feladatgyűjtemény, ahol nincs elmélet, csak feladatok, megoldókulcsokkal. Észrevételeket, javaslatokat a <b>zsebtanar@gmail.com</b>-ra lehet küldeni. Jó tanulást! <i>Szabó Viktor</i></p>
 				<div class="pull-left"><a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/deed.hu" target="_blank"><img alt="Creative Commons Licenc" style="border-width:0" src="https://i.creativecommons.org/l/by-nc-sa/4.0/88x31.png" /></a></div>
+				
 				<button type="button" class="pull-right btn btn-default" data-dismiss="modal">Bezár</button>
 			</div>
 		</div>
