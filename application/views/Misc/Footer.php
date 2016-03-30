113e8d827b637e422cc81deeceff1fd8af2eee0b
Zsebtanár <zsebtanar@gmail.com>
2016-03-31 01:48:41 +0200

    cookie script removed (temporarily) Signed-off-by: Zsebtanár <zsebtanar@gmail.com>

--- application/views/Misc/Footer.php
+++ application/views/Misc/Footer.php
@@ -1,4 +1,4 @@
 <!-- Auto resize input -->
 <script type="text/javascript" src="<?php echo base_url();?>assets/js/jquery.autosize.input.js"></script>
 <!--Cookie Script-->
-<script type="text/javascript" charset="UTF-8" src="<?php echo base_url();?>assets/js/cookie.js"></script>
+<!-- <script type="text/javascript" charset="UTF-8" src="<?php // echo base_url();?>assets/js/cookie.js"></script>-->
