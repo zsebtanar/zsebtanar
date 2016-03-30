186e1fa4f1cf6bb4680ff8c052b317c96d5538ed
Zsebtanar <zsebtanar@gmail.com>
2016-03-30 22:19:45 +0200

    drawtext, drawcircle Signed-off-by: Zsebtanar <zsebtanar@gmail.com>

--- application/helpers/draw_helper.php
+++ application/helpers/draw_helper.php
@@ -7,4 +7,17 @@ function DrawLine($x1, $y1, $x2, $y2, $color='black', $width=1) {
   return $svg;
 }
 
+function DrawText($x, $y, $text, $fontsize=10, $color='black', $transform='') {
+
+  $svg = '<text font-size="'.$fontsize.'" x="'.$x.'" y="'.$y.'" fill="'.$color.'" transform="'.$transform.'">'.$text.'</text>';
+
+  return $svg;
+}
+
+function DrawCircle($cx, $cy, $r, $color1='black', $width=1, $color2='none') {
+
+  $svg = '<circle cx="'.$cx.'" cy="'.$cy.'" r="'.$r.'" stroke="'.$color1.'" stroke-width="'.$width.'" fill="'.$color2.'" />';
+
+  return $svg;
+}
 ?>
