411a43cb568d8f0740a8003f9b0fc2e62bc2840e
Zsebtanar <zsebtanar@gmail.com>
2016-04-21 18:48:50 +0200

    exercise (12/2015_10_13/Tigris) DONE Signed-off-by: Zsebtanar <zsebtanar@gmail.com>

---
+++ application/libraries/11/Kombinatorika/Kombinacio.php
@@ -0,0 +1,119 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Kombinacio {
+
+	// Class constructor
+	function __construct() {
+
+		$CI =& get_instance();
+		$CI->load->helper('maths');
+		$CI->load->helper('language');
+		$CI->load->helper('draw');
+
+		return;
+	}
+
+	function Generate($level) {
+
+		if ($level <= 3) {
+			$male 	= rand(2,3);
+			$female = $male + rand(1,2);
+		} elseif ($level <= 6) {
+			$male 	= rand(3,4);
+			$female = $male + rand(2,3);
+		} else {
+			$male 	= rand(4,5);
+			$female = $male + rand(3,4);
+		}
+
+		$total	= $male + $female;
+		$min 	= $male-1;
+
+		$question = 'Egy állatkert a tigrisek fennmaradása érdekében tenyésztő programba kezd. Beszereznek $'.$male.'$ hím és $'.$female.'$ nőstény kölyöktigrist, melyeket egy kisebb és egy nagyobb kifutóban kívánnak elhelyezni a következő szabályok mindegyikének betartásával:
+			<ol type="I">
+				<li>'.NumText($min).By($min).' kevesebb tigris egyik kifutóban sem lehet;</li>
+				<li>a nagyobb kifutóba több tigris kerül, mint a kisebbikbe;</li>
+				<li>mindkét kifutóban hím és nőstény tigrist is el kell helyezni;</li>
+				<li>egyik kifutóban sem lehet több hím, mint nőstény tigris.</li>
+			</ol>
+			Hányféleképpen helyezhetik el a $'.$total.'$ tigrist a két kifutóban?<br />(A tigriseket megkülönböztetjük egymástól, és két elhelyezést eltérőnek tekintünk, ha van olyan tigris, amelyik az egyik elhelyezésben más kifutóban van, mint a másik elhelyezésben.)'
+		;
+
+		for ($i=$min; $i <= $total; $i++) { 
+			
+			if ($i == $min) {
+				$page[] = 'Válogassuk szét az eseteket aszerint, hogy a kisebbik kifutóban hány tigris van!';
+			} else {
+				$page = [];	
+			}
+			
+			$page[] = '<b>'.strval($i-$min+1).'. eset:</b> a kisebbik kifutóban $'.$i.'$ tigris van, a nagyobbikban pedig $'.strval($total-$i).'$.'.($i == $min ? ' (Ennél kevesebb az I) feltétel miatt sehol sem lehet.)' : '');
+
+			if ($i >= $total-$i) {
+
+				$page[] = 'Ez az eset azért <span class="label label-danger">nem jó</span>, mert a II) feltétel szerint a kisebb kifutóban kevesebb tigrisnek kell lennie, mint a nagyobbikban. (És itt meg is állhatunk, mert ha tovább növeljük a tigrisek számát, ez a feltétel úgy sem fog teljesülni.)';
+				$hints[] = $page;
+				break;
+
+			} 
+
+			$page[] = 'Most válogassuk szét az eseteket aszerint, hogy '.The($i).' $'.$i.'$ tigris közül hány nőstény!';
+
+
+			for ($j=0; $j <= $i; $j++) {
+
+				$text = '<ul><li><i>'.strtoupper(The($i)).' $'.$i.'$ tigris közül $'.$j.'$ hím és $'.strval($i-$j).'$ nőstény.</i><br />';
+
+				if ($i == 0 || $j == 0) {
+
+					$text .= 'Ez az eset azért <span class="label label-danger">nem jó</span>, mert a III) feltétel miatt nősténynek és hímnek is kell lennie a kifutóban.';
+
+				} elseif ($j > $i-$j) {
+
+					$text .= 'Ez az eset azért <span class="label label-danger">nem jó</span>, mert a IV) feltétel miatt nem lehet a kifutóban több hím, mint nőstény.';
+
+				} elseif ($male-$j > $female-$i+$j) {
+
+					$text .= 'Ez az eset azért <span class="label label-danger">nem jó</span>, mert ha ez azt jelentené, hogy a másik kifutóban $'.strval($male-$j).'$ hím és $'.strval($female-$i+$j).'$ nőstény van, viszont a IV) feltétel miatt nem lehet a kifutóban több hím, mint nőstény.';
+
+				} else {
+
+					$female_opt = binomial_coeff($female, $i-$j);
+					$male_opt	= binomial_coeff($male, $j);
+					$answers[] 	= $female_opt * $male_opt;
+
+					$text .= 'Ez az eset <span class="label label-success">jó</span>, mert minden feltételnek megfelel.
+						<ul>
+							<li>$'.$male.'$ hím közül $'.$j.'$-'.Dativ($j).' összesen ${'.$male.'\choose '.$j.'}='.$male_opt.'$-féleképpen lehet kiválasztani;</li>
+							<li>$'.$female.'$ nőstény közül $'.strval($i-$j).'$-'.Dativ($i-$j).' összesen ${'.$female.'\choose '.strval($i-$j).'}='.$female_opt.'$-féleképpen lehet kiválasztani;</li>
+							<li>Ha már tudjuk, hogy a kisebbik kifutóban melyik tigrisek vannak, akkor már azt is tudjuk, hogy a nagyobbikban melyikek vannak, tehát ezt nem kell külön kiszámolni.</li>
+							<li>Ebben az esetben összesen $'.$male_opt.'\cdot'.$female_opt.'=$<span class="label label-info">$'.strval($female_opt*$male_opt).'$</span> különböző lehetőség.</li>
+						</ul>';
+				}
+
+				$text .= '</li></ul>';
+				$page[] = $text;
+
+			}
+
+			$hints[] = $page;
+		}
+
+		$page = [];
+		$page[] = 'Tehát összesen $'.implode('+', $answers).'=$<span class="label label-success">$'.array_sum($answers).'$</span>-féleképpen lehet elhelyezni a tigriseket a két kifutóban.';
+		$hints[] = $page;
+
+		$correct = array_sum($answers);
+		$solution = '$'.$correct.'$';
+
+		return array(
+			'question'  => $question,
+			'correct'   => $correct,
+			'solution'  => $solution,
+			'hints'		=> $hints
+		);
+	}
+}
+
+?>
--- application/libraries/12/2015_10_13/Tigris.php
+++ application/libraries/12/2015_10_13/Tigris.php
@@ -16,46 +16,26 @@ class Tigris {
 
 	function Generate($level) {
 
-		$year_diff 	= rand($level, 2*$level);
-		$year2 		= 2014 + $year_diff;
+		$CI =& get_instance();
 
-		$a = rand($level, 5*$level)*100;
-		$b = rand(75, 95)/100;
-		$cc = pow($b, $year_diff);
-		$c = round(pow($b, $year_diff)*100)/100;
-		$d = $a*$c;
+		if ($level <= 3) {
 
-		$b2 = str_replace('.', ',', $b);
-		$c2 = str_replace('.', ',', $c);
-		$d2 = str_replace('.', ',', $d);
-		
-		$question = 'Egy $2014$ végén készült előrejelzés szerint az Indiában élő tigrisek $t$ száma az elkövetkező években (az egyes évek végén) megközelítőleg a következő összefüggés szerint alakul: $t(x) = '.$a.' \cdot '.$b2.'^x$, ahol $x$ a $2014$ óta eltelt évek számát jelöli. Számítsa ki, hogy az előrejelzés alapján $'.$year2.'$ végére hány százalékkal csökken a tigrisek száma a $2014$-es év végi adathoz képest! A megoldást egész számokban adja meg!';
+			$CI->load->library('11/Hatvany/Exponencialis_egyenlet', NULL, 'Exponencialis_egyenlet');
+			$data = $CI->Exponencialis_egyenlet->Generate($level);
 
-		$page[] = '<b>1. megoldás:</b> Ha az $x$ helyére $0$-t írunk, megkapjuk, hogy a tigrisek számát $2014$-ben:$$t(0)='.$a.'\cdot'.$b2.'^0='.$a.'\cdot1='.$a.'$$';
-		$page[] = '$2014$ és $'.$year2.'$ között összesen $'.$year2.'-2014='.$year_diff.'$ év telik el.';
-		$page[] = 'Ezért ha az $x$ helyére $'.$year_diff.'$-'.Dativ($year_diff).' írunk, megkapjuk, hogy az előrejelzés szerint $'.$year2.'$-'.In($year2).' hány tigris lesz:'
-			.'$$t('.$year_diff.')='.$a.'\cdot'.$b2.'^{'.$year_diff.'}'.($c == $cc ? '=' : '\approx').$a.'\cdot'.$c2.'='.$d2.'$$';
-		$page[] = 'Osszuk el a $t('.$year_diff.')$-'.Dativ($year_diff).' elosztjuk $t(0)$-lal:'
-			.'$$\frac{t('.$year_diff.')}{t(0)}=\frac{'.$d.'}{'.$a.'}='.$c2.'$$';
-		$page[] = 'Ez azt jelenti, hogy $'.$year2.'$-'.In($year2).' a tigrisek száma a $2014$-hez képest csak $'.strval($c*100).'\%$ lesz, vagyis a számuk $100\%-'.strval($c*100).'\%=$<span class="label label-success">$'.strval(100*(1-$c)).'$</span>$\%$-kal csökken.';
-		$hints[] = $page;
+		} elseif ($level <= 6) {
 
-		$page = [];
-		$page[] = '<b>2. megoldás:</b> A tigrisek száma minden évben az előző évinek $'.$b2.'$-'.Times2($b*100).' változik.';
-		$page[] = 'Ekkor $'.$year_diff.'$ év alatt a változás $'.$b2.'^'.$year_diff.($c == $cc ? '=' : '\approx').$c2.'$ lesz.';
-		$page[] = 'Azaz, a tigrisek száma $100\%-'.strval($c*100).'\%=$<span class="label label-success">$'.strval(100*(1-$c)).'$</span>$\%$-kal csökken.';
-		$hints[] = $page;
+			$CI->load->library('11/Hatvany/Exponencialis_egyenlet2', NULL, 'Exponencialis_egyenlet2');
+			$data = $CI->Exponencialis_egyenlet2->Generate($level);
 
-		$correct = round(100*(1-pow($b, $year_diff)));
-		$solution = '$'.strval(round((1-$c)*100)).'$';
+		} else {
 
-		return array(
-			'question'  => $question,
-			'correct'   => $correct,
-			'solution'  => $solution,
-			'labels'	=> ['right' => '$\%$'],
-			'hints'		=> $hints
-		);
+			$CI->load->library('11/Kombinatorika/Kombinacio', NULL, 'Kombinacio');
+			$data = $CI->Kombinacio->Generate($level);
+
+		}
+
+		return $data;
 	}
 }
 
