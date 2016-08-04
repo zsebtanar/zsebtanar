f5c7a6bb42b7bc6402f2d097dd697ffa8b976db1
Zsebtanar <zsebtanar@gmail.com>
2016-08-04 17:35:03 +0200

    left label added to input field Signed-off-by: Zsebtanar <zsebtanar@gmail.com>

--- application/views/Input/Default.php
+++ application/views/Input/Default.php
@@ -3,7 +3,7 @@
 		<tbody>
 			<tr>
 				<td align="center">
-					<input type="text" align="center" class="form-control form-control-inline smallInput" autofocus="autofocus" data-autosize-input='{ "space": 20 }' name="answer"><?php if (isset($labels['right'])) { echo $labels['right'];} ?>
+					<?php if (isset($labels['left'])) { echo $labels['left'];} ?><input type="text" align="center" class="form-control form-control-inline smallInput" autofocus="autofocus" data-autosize-input='{ "space": 20 }' name="answer"><?php if (isset($labels['right'])) { echo $labels['right'];} ?>
 				</td>
 			</tr>
 		</tbody>
