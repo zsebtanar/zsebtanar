624fb44b7b1b7aa70def1d3aca7b1bf612423168
Zsebtanar <zsebtanar@gmail.com>
2016-08-05 18:44:16 +0200

    minor change ("$" added) Signed-off-by: Zsebtanar <zsebtanar@gmail.com>

--- application/views/Input/Range.php
+++ application/views/Input/Range.php
@@ -3,8 +3,8 @@
 		<tbody>
 			<tr>
 				<td align="center">
-					[<input type="text" align="center" autofocus="autofocus" class="form-control form-control-inline smallInput" data-autosize-input='{ "space": 20 }' name="answer">;
-					<input type="text" align="center" class="form-control form-control-inline smallInput" data-autosize-input='{ "space": 20 }' name="answer">]
+					$[$<input type="text" align="center" autofocus="autofocus" class="form-control form-control-inline smallInput" data-autosize-input='{ "space": 20 }' name="answer">$;$
+					<input type="text" align="center" class="form-control form-control-inline smallInput" data-autosize-input='{ "space": 20 }' name="answer">$]$
 				</td>
 			</tr>
 		</tbody>
