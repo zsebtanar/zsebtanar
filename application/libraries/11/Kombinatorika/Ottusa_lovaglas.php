6e9417cb1ca8cd9fe7a2a86a30d9268e8a75ddff
Zsebtanar <zsebtanar@gmail.com>
2016-08-10 23:59:31 +0200

    rename exercise (ottusa -> ottusa_lovaglas) Signed-off-by: Zsebtanar <zsebtanar@gmail.com>

--- application/libraries/11/Kombinatorika/Ottusa_lovaglas.php
+++ application/libraries/11/Kombinatorika/Ottusa_lovaglas.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Ottusa {
+class Ottusa_lovaglas {
 
 	// Class constructor
 	function __construct() {
@@ -21,16 +21,21 @@ class Ottusa {
 		$B = rand(2,5);
 		$C = 12-$A-$B;
 
+		// // Original exercise
+		// $A = 5;
+		// $B = 4;
+		// $C = 3;
+
 		$question = 'Az öttusa lovaglás számában egy akadálypályán tizenkét különböző akadályt kell a versenyzőnek átugratnia. Egy akadály a nehézsége alapján három csoportba sorolható: $A$, $B$ vagy $C$ típusú. Ádám a verseny előtti bemelegítéskor először '.The($A).' '.NumText($A).' darab $A$, majd '.The($B).' '.NumText($B).' darab $B$, végül '.The($C).' '.NumText($C).' darab $C$ típusú akadályon ugrat át, mindegyiken pontosan egyszer. Bemelegítéskor az egyes akadálytípusokon belül a sorrend szabadon megválasztható. Számítsa ki, hogy a bemelegítés során hányféle sorrendben ugrathatja át Ádám a tizenkét akadályt!';
 		$correct = fact($A)*fact($B)*fact($C);
-		$solution = '$'.$correct.'$';
+		$solution = '$'.round2($correct,0).'$';
 		$type = 'int';
 
 		$page[] = 'Az $A$ típusú akadályok lehetséges sorrendjeinek a száma $'.$A.'!='.fact($A).'$.';
 		$page[] = 'A $B$ típusú akadályok lehetséges sorrendjeinek a száma $'.$B.'!='.fact($B).'$.';
 		$page[] = 'A $C$ típusú akadályok lehetséges sorrendjeinek a száma $'.$C.'!='.fact($C).'$.';
 		$page[] = 'A $12$ akadály lehetséges sorrendjeinek a száma ezek szorzata.';
-		$page[] = 'Tehát Ádám összesen $'.fact($A).'\cdot'.fact($B).'\cdot'.fact($C).'=$<span class="label label-success">$'.$correct.'$</span> különböző sorrendben ugrathatja át a tizenkét akadályt.';
+		$page[] = 'Tehát Ádám összesen $'.fact($A).'\cdot'.fact($B).'\cdot'.fact($C).'=$<span class="label label-success">$'.round2($correct,0).'$</span> különböző sorrendben ugrathatja át a tizenkét akadályt.';
 		$hints[] = $page;
 
 		return array(
