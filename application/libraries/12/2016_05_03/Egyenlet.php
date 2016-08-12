c9676519f7b28fc10038aa51736a01dc15539272
Zsebtanar <zsebtanar@gmail.com>
2016-08-12 13:17:59 +0200

    rename exercise Signed-off-by: Zsebtanar <zsebtanar@gmail.com>

--- application/libraries/12/2016_05_03/Egyenlet.php
+++ application/libraries/12/2016_05_03/Egyenlet.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Egyenlotlenseg {
+class Egyenlet {
 
 	// Class constructor
 	function __construct() {
@@ -16,7 +16,17 @@ class Egyenlotlenseg {
 	function Generate($level) {
 
 		$CI =& get_instance();
-		$CI->load->library('10/Masodfoku_egyenlet/Egyenlotlenseg_grafikus', NULL, 'Egyenlet');
+
+		if ($level <= 4) {
+
+			$CI->load->library('9/Egyenletek/Tortek', NULL, 'Egyenlet');
+
+		} else {
+
+			$CI->load->library('10/Masodfoku_egyenlet/Egyenlotlenseg_grafikus', NULL, 'Egyenlet');
+			
+		}
+		
 		$data = $CI->Egyenlet->Generate($level);
 
 		return $data;
