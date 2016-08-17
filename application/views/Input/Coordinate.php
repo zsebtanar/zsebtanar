f04320f3027753adb43c8f96c1b43a42e92da2ec
Zsebtanar <zsebtanar@gmail.com>
2016-08-17 16:26:04 +0200

    multiple coordinates can also be handled Signed-off-by: Zsebtanar <zsebtanar@gmail.com>

--- application/views/Input/Coordinate.php
+++ application/views/Input/Coordinate.php
@@ -2,12 +2,23 @@
 	<table align="center">
 		<tbody><?php
 
-			foreach ($labels as $label) {?>
+			if (count($labels) > 1) {
 
-			<tr>
-				<td align="center"><?php echo $label; ?></td>
-				<td align="center">(<input type="text" align="center" autofocus="autofocus" class="form-control form-control-inline smallInput" data-autosize-input='{ "space": 20 }' name="answer">;<input type="text" align="center" autofocus="autofocus" class="form-control form-control-inline smallInput" data-autosize-input='{ "space": 20 }' name="answer">)</td>
-			</tr><?php
+				foreach ($labels as $label) {?>
+
+				<tr>
+					<td align="center"><?php echo $label; ?></td>
+					<td align="center">(<input type="text" align="center" autofocus="autofocus" class="form-control form-control-inline smallInput" data-autosize-input='{ "space": 20 }' name="answer">;<input type="text" align="center" autofocus="autofocus" class="form-control form-control-inline smallInput" data-autosize-input='{ "space": 20 }' name="answer">)</td>
+				</tr><?php
+
+				}
+				
+			} else {?>
+
+				<tr>
+					<td align="center"><?php echo $labels; ?></td>
+					<td align="center">(<input type="text" align="center" autofocus="autofocus" class="form-control form-control-inline smallInput" data-autosize-input='{ "space": 20 }' name="answer">;<input type="text" align="center" autofocus="autofocus" class="form-control form-control-inline smallInput" data-autosize-input='{ "space": 20 }' name="answer">)</td>
+				</tr><?php
 
 			}?>
 
