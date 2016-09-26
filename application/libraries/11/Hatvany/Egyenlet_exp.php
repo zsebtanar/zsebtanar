0eb12a37c088c4f8f6124433f0a1789101a54b15
Zsebtanar <zsebtanar@gmail.com>
2016-09-26 19:59:16 +0200

    BUG fixed (round2() was used for correct answer instead of round1()) Signed-off-by: Zsebtanar <zsebtanar@gmail.com>

--- application/libraries/11/Hatvany/Egyenlet_exp.php
+++ application/libraries/11/Hatvany/Egyenlet_exp.php
@@ -35,8 +35,8 @@ class Egyenlet_exp {
 
 		$exponent1 = log10($result)/log10($base);
 		$exponent2 = round2($exponent1, 5);
-		$correct = round2($exponent1, 3);
-		$solution = '$'.$correct.'$';
+		$correct = round1($exponent1, 3);
+		$solution = '$'.round2($correct).'$';
 
 		$page[] = 'Vegyük mindkét oldal $10$-es alapú logaritmusát:$$\log_{10}\left('.$base.'^x\right)=\log_{10}'.$result.'$$';
 		$page[] = '<div class="alert alert-info"><strong>Logaritmus azonossága:</strong><br />Hatvány logaritmusa egyenlő az alap logaritmusának és a kitevőnek a szorzatával:$$\log_ab^k=k\cdot\log_ab$$</div>';
@@ -46,7 +46,7 @@ class Egyenlet_exp {
 		$page = [];
 		$page[] = 'Osszuk el mindkét oldalt $\log_{10}'.$base.'$-'.With($base).'!$$x=\frac{\log_{10}'.$result.'}{\log_{10}'.$base.'}\approx'.$exponent2.'$$';
 		$page[] = '<b>Megjegyzés</b>: a számológépen a tízes alapú logaritmust a <b>log</b> gomb jelöli:<div class="text-center"><kbd>'.$result.'</kbd> <kbd>log</kbd> <kbd>&divide;</kbd> <kbd>'.$base.'</kbd>  <kbd>log</kbd> <kbd>=</kbd></div>';
-		$page[] = 'A megoldás három tizedesjegyre kerekített értéke <span class="label label-success">$'.$correct.'$</span>.';
+		$page[] = 'A megoldás három tizedesjegyre kerekített értéke <span class="label label-success">$'.round2($correct).'$</span>.';
 		$hints[] = $page;
 
 		return array(
