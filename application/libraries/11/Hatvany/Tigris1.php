363cb929b5890075c5b17362cfcb45f5f389a8b2
Zsebtanar <zsebtanar@gmail.com>
2016-10-21 02:06:41 +0200

    BUG FIXED: constructor name corrected Signed-off-by: Zsebtanar <zsebtanar@gmail.com>

--- application/libraries/11/Hatvany/Tigris1.php
+++ application/libraries/11/Hatvany/Tigris1.php
@@ -3,8 +3,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Tigris1 {
 
-	// Class tiger1ructor
-	function __tiger1ruct() {
+	// Class constructor
+	function __construct() {
 
 		$CI =& get_instance();
 		$CI->load->helper('maths');
