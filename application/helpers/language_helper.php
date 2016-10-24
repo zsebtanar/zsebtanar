0beec3e429c523df8852d7163e053c2a61ec2193
Zsebtanar <zsebtanar@gmail.com>
2016-10-25 01:59:46 +0200

    NEW FUNCTION: slugify text Signed-off-by: Zsebtanar <zsebtanar@gmail.com>

--- application/helpers/language_helper.php
+++ application/helpers/language_helper.php
@@ -748,4 +748,38 @@ function BigNum($num) {
 
   return $num2;
 }
+
+/**
+ * Slugify text
+ *
+ * @param string $text Text
+ *
+ * @return string $text Text (formatted)
+ */
+function slugify($text)
+{
+  // replace non letter or digits by -
+  $text = preg_replace('~[^\pL\d]+~u', '-', $text);
+
+  // transliterate
+  $text = iconv('utf-8', 'us-ascii//TRANSLIT', $text);
+
+  // remove unwanted characters
+  $text = preg_replace('~[^-\w]+~', '', $text);
+
+  // trim
+  $text = trim($text, '-');
+
+  // remove duplicate -
+  $text = preg_replace('~-+~', '-', $text);
+
+  // lowercase
+  $text = strtolower($text);
+
+  if (empty($text)) {
+    return 'n-a';
+  }
+
+  return $text;
+}
 ?>
