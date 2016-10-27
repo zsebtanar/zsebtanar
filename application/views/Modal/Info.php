a2d654b64c79cef84550c246ec462f8ca923c11e
Zsebtanar <zsebtanar@gmail.com>
2016-10-27 23:08:12 +0200

    info modal size decreased Signed-off-by: Zsebtanar <zsebtanar@gmail.com>

--- application/views/Modal/Info.php
+++ application/views/Modal/Info.php
@@ -1,25 +1,28 @@
 <div id="info" class="modal fade" role="dialog">
 	<div class="modal-dialog">
 		<div class="modal-content">
-			<div class="modal-body">
+			<div class="modal-header small">
 				<button type="button" class="close" data-dismiss="modal">&times;</button>
-				<h1 class="modal-title">Jelmagyarázat</h1>
-				<h3><b><img class="img_main" src="<?php echo base_url();?>assets/images/coin.png" alt="coin" width="40px">&nbsp;Arany</b></h3>
+				<h3 class="modal-title">Jelmagyarázat</h3>
+			</div>
+			<div class="modal-body small">
+				
+				<h4><b><img class="img_main" src="<?php echo base_url();?>assets/images/coin.png" alt="coin" width="25px">&nbsp;Arany</b></h4>
 				<ul>
 					<li>Minden jó feladatmegoldásért kapsz aranyat.</li>
 					<li>Minél kevesebb segítséget használsz fel a megoldáshoz, annál többet.</li>
 					<li>Ha szintet lépsz, vagy elvégzel egy feladatot, extra aranyat kapsz.</li>
 				</ul>
-				<h3><b><img class="img_main" src="<?php echo base_url();?>assets/images/shield.png" alt="shield" width="40px">&nbsp;Pajzs</b></h3>
+				<h4><b><img class="img_main" src="<?php echo base_url();?>assets/images/shield.png" alt="shield" width="25px">&nbsp;Pajzs</b></h4>
 				<ul>
 					<li>Ha szintet lépsz egy feladatnál, egy pajzsot kapsz.</li>
 				</ul>
-				<h3><b><img class="img_main" src="<?php echo base_url();?>assets/images/trophy.png" alt="trophy" width="40px">&nbsp;Trófea</b></h3>
+				<h4><b><img class="img_main" src="<?php echo base_url();?>assets/images/trophy.png" alt="trophy" width="25px">&nbsp;Trófea</b></h4>
 				<ul>
 					<li>Ha elvégzel egy feladatot, egy trófeát kapsz jutalomként.</li>
 				</ul>
 				<br />
-				<h1 class="modal-title">Gyorsbillentyűk</h1>
+				<h3 class="modal-title">Gyorsbillentyűk</h3>
 				<ul>
 					<li>Eredmény elküldése: <kbd class="lg">Enter</kbd></li>
 					<li>Következő segítség: <kbd class="lg">&#8592;</kbd></li>
@@ -27,7 +30,7 @@
 				</ul>
 			</div>
 
-			<div class="modal-footer">
+			<div class="modal-footer small">
 				<button type="button" class="pull-right btn btn-default" data-dismiss="modal">Bezár</button>
 			</div>
 		</div>
