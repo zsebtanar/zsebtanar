f25a6a08850b3fe9bc055d9f974137c5c2e2679c
Zsebtanar <zsebtanar@gmail.com>
2016-10-27 23:08:12 +0200

    bootstrap theme-min added & typeahead.css removed Signed-off-by: Zsebtanar <zsebtanar@gmail.com>

--- application/views/Misc/Header.php
+++ application/views/Misc/Header.php
@@ -18,20 +18,16 @@
 <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.css"> -->
 <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.css"> -->
 <link rel="stylesheet" href="<?php echo base_url();?>assets/css/bootstrap.css">
-<!-- <link rel="stylesheet" href="<?php // echo base_url();?>assets/css/bootstrap-theme.css"> -->
+<link rel="stylesheet" href="<?php echo base_url();?>assets/css/bootstrap-theme.css">
 <link rel="stylesheet" href="<?php echo base_url();?>assets/css/bootstrap_mod.css">
 <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Open+Sans:400,300,700&amp;subset=latin,latin-ext">
 
-<!-- Typeahead CSS -->
-<link rel="stylesheet" href="<?php echo base_url();?>assets/css/typeahead.css">
+<!-- JQuery CSS -->
 <link rel="stylesheet" href="<?php echo base_url();?>assets/css/jquery-ui.css">
 
 <!-- JQuery JS -->
-<!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.js"></script> -->
-<!-- <script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.js"></script> -->
 <script src="<?php echo base_url();?>assets/js/jquery.js"></script>
 <script src="<?php echo base_url();?>assets/js/jquery-ui.js"></script>
-
 
 <!-- Bootstrap JS -->
 <!-- <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.js"></script> -->
