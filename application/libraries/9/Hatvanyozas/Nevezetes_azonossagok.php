3aecdd08d75a6c0b56c1e866777c25d58e9b14a2
Zsebtanar <zsebtanar@gmail.com>
2016-11-10 11:50:31 +0100

    rename exercise (nevezetes azonosságok) Signed-off-by: Zsebtanar <zsebtanar@gmail.com>

--- application/libraries/9/Hatvanyozas/Nevezetes_azonossagok.php
+++ application/libraries/9/Hatvanyozas/Nevezetes_azonossagok.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Nevezetes_azonossag {
+class Nevezetes_azonossagok {
 
 	// Class constructor
 	function __construct() {
