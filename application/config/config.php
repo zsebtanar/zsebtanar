7e93d4a0bfbd462ebfecc5e0726d9267198e4c3e
Zsebtanar <zsebtanar@gmail.com>
2016-11-11 02:28:09 +0100

    change uri protocol Signed-off-by: Zsebtanar <zsebtanar@gmail.com>

--- application/config/config.php
+++ application/config/config.php
@@ -54,7 +54,7 @@ $config['index_page'] = '';
 |
 | WARNING: If you set this to 'PATH_INFO', URIs will always be URL-decoded!
 */
-$config['uri_protocol']	= 'PATH_INFO';
+$config['uri_protocol']	= 'REQUEST_URI'; // Use 'PATH_INFO' for zsebtanar.hu!!!
 
 /*
 |--------------------------------------------------------------------------
