9abb5ebe29b602c34b9610efd23fd6fa43a5dc36
Zsebtanar <zsebtanar@gmail.com>
2017-01-12 23:47:01 +0100

    remove extra leading spaces Signed-off-by: Zsebtanar <zsebtanar@gmail.com>

--- application/helpers/language_helper.php
+++ application/helpers/language_helper.php
@@ -773,9 +773,9 @@ function slugify($text) {
  * @return string $text Text (formatted)
  */
 function mb_ucfirst($string, $encoding='UTF8') {
-    $strlen = mb_strlen($string, $encoding);
-    $firstChar = mb_substr($string, 0, 1, $encoding);
-    $then = mb_substr($string, 1, $strlen - 1, $encoding);
-    return mb_strtoupper($firstChar, $encoding) . $then;
+  $strlen = mb_strlen($string, $encoding);
+  $firstChar = mb_substr($string, 0, 1, $encoding);
+  $then = mb_substr($string, 1, $strlen - 1, $encoding);
+  return mb_strtoupper($firstChar, $encoding) . $then;
 }
 ?>
