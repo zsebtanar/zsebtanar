8a168824a246c6039b1efe759a255f35e34eafa5
Zsebtanar <zsebtanar@gmail.com>
2017-01-12 23:46:37 +0100

    change description of website Signed-off-by: Zsebtanar <zsebtanar@gmail.com>

--- application/views/Misc/Header.php
+++ application/views/Misc/Header.php
@@ -1,7 +1,7 @@
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<meta name="description" content="Ingyenes matematika oktatóvideók">
+<meta name="description" content="Interaktív matematika feladatgyűjtemény">
 <meta name="author" content="Szabó Viktor">
 <meta name="google-site-verification" content="zDMT36c6WVy68sDdP-1BBbCVeAYEu2P3RUTXkHyWVDw" />
 
--- application/views/Misc/Header2.php
+++ application/views/Misc/Header2.php
@@ -1,7 +1,7 @@
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<meta name="description" content="Ingyenes matematika oktatóvideók">
+<meta name="description" content="Interaktív matematika feladatgyűjtemény">
 <meta name="author" content="Szabó Viktor">
 <meta charset="UTF-8">
 <link rel="icon" type="image/png" href="<?php echo base_url();?>assets/images/logo_small.png">
