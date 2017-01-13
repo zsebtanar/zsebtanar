7c962ead59cada5a3a41bc7f3f3256f4cba776e5
Zsebtanar <zsebtanar@gmail.com>
2017-01-13 01:47:09 +0100

    BUG fixed: use png instead of svg (Chrome doesn't display last letter) Signed-off-by: Zsebtanar <zsebtanar@gmail.com>

--- application/views/Title/Main.php
+++ application/views/Title/Main.php
@@ -1,7 +1,7 @@
 <div class="jumbotron text-center" id="top">
 	<h1>
 		<a href="<?php echo base_url().'view/main';?>" class="nounderline text-center">
-			<img class="img-main" src="<?php echo base_url();?>assets/images/emblem.svg" alt="Logo">
+			<img class="img-main" src="<?php echo base_url();?>assets/images/emblem.png" alt="Logo">
 		</a>
 	</h1>
 </div>
